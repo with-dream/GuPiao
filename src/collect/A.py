dd7c2f8e71c814570524b6679f0024f708d855f3
with-dream <854415002@qq.com>
2021-09-07 00:51:45 +0800

    打印

--- src/collect/A.py
+++ src/collect/A.py
@@ -1,3 +1,12 @@
 import akshare as ak
 stock_info_a_code_name_df = ak.stock_info_a_code_name()
-print(stock_info_a_code_name_df)
+ss = set()
+
+for index in stock_info_a_code_name_df.index:
+    value = stock_info_a_code_name_df.loc[index]
+    code = value.values[0]
+    name = value.values[1]
+    print(name)
+    ss.add(code[:3])
+
+print(ss)
